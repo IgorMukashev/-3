0b12919f2bedb9579c3e7b2f6a6364cfb5cd7aba
IgorMukashev <96055320+IgorMukashev@users.noreply.github.com>
2022-01-22 20:13:47 +0300

    урок 3
    домашка

---
+++ Урок 3 упражнение2.1.py
@@ -0,0 +1,4 @@
+# def my_func(name, surname, byear, city, email, phone):
+#     print(name, surname, byear, city, email, phone)
+#
+# my_func(name= "Igor", surname="Muk", byear="1987", city="Msc", email="mukashevis@rambler.ru", phone="7227")
---
+++ урок 3 упражнение 1.py
@@ -0,0 +1,12 @@
+# def devision (a, b) :
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#       return "На ноль делить нельзя"
+#     except TupeError:
+#       return "Не верные входные данные"
+#
+# print(devision( 3, 4))
+# print(devision( 13, 0))
+# print(devision( 65, 5))
+# print(devision( 65, 5))
